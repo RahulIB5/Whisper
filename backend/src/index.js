61a3b26b1ec9ee7cd850cb72e4c2c81c342f03ac
Rahul IB <rahulib7172@gmail.com>
2025-03-15 14:28:34 +0530

    Build path corrected

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { io, app, server } from "./lib/socket.js";
 
+
 dotenv.config()
 
 const PORT = process.env.PORT;
@@ -29,13 +30,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "../../frontend", "dist", "index.html"));
     });
 }
-
 
 
 server.listen(PORT, () => {
